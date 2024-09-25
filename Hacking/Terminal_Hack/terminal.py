4b866d69065c2554e913e670c81acf3fccba767e
cybertejas <tejascyberethics@gmail.com>
2024-09-25 23:35:17 +0530

    .changes_made_venv

---
+++ Hacking/Terminal_Hack/terminal.py
@@ -0,0 +1,30 @@
+import os
+import pyfiglet
+import time
+
+a = 'clear' # in windows, change this into cls
+
+
+
+os.system(a)
+
+ascii_banner = pyfiglet.figlet_format("Welcome To Forced Terminal")
+print(ascii_banner)
+
+time.sleep(3)
+
+try:
+    while True:
+            enter_command = input("\n\nEnter a command:-> ")
+            os.system(enter_command)
+            if enter_command == 'clear':
+                ascii_banner = pyfiglet.figlet_format("Welcome To Forced Terminal")
+                print(ascii_banner)
+
+except KeyboardInterrupt:
+    time.sleep(1)
+    os.system(a)
+    ascii_banner = pyfiglet.figlet_format("To Quit, Close The Window")
+    print(ascii_banner)
+    time.sleep(1)
+    os.system(a)
